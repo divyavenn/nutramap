664ec8b591e1e86dddb58f44602be58e22017ec8
Divya Vennan <divyavenkatraman234@gmail.com>
2024-11-21 00:08:01 -0500

    reset

--- backend/databases/main_connection.py
+++
@@ -1,6 +0,0 @@
-from .food_data_connect import Base, engine, get_session
-from .user_data_connect import close_mongo_db, get_user_data
-from .food_models import Nutrient, Food, Data
-from .user_models_mongo import User, UserCreate, Log, LogCreate, Requirement, RequirementCreate
-
-__package__ = "nutramap.backend.databases"
--- install_reqs.sh
+++
@@ -1,6 +0,0 @@
-conda install -c conda-forge --yes --file requirements.txt
-conda install -c conda-forge fastapi
-conda install -c conda-forge uvicorn
-conda install -c conda-forge sqlalchemy
-conda install -c conda-forge typing-extensions
-conda install -c conda-forge pymongo
